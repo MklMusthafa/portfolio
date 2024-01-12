c20f40ab2a3b6337628c508f24fe291ee29519ec
MklMusthafa <musthafa.m11111@gmail.com>
2024-01-12 20:12:54 +0530

    fix: quote escape

--- app/sections/about/index.jsx
+++ app/sections/about/index.jsx
@@ -24,35 +24,35 @@ export function AboutSection() {
 						}}
 					>
 						<p className="my-3.5">
-							Hello there! I'm Musthafa M, a passionate and results-driven software engineer with a
+							{`Hello there! I'm Musthafa M, a passionate and results-driven software engineer with a
 							keen eye for innovation and problem-solving. With a solid background in computer
 							science and a knack for turning complex ideas into efficient, user-friendly solutions,
-							I thrive on the challenges that come with crafting cutting-edge software.
+							I thrive on the challenges that come with crafting cutting-edge software.`}
 						</p>
 						<p className="my-3.5">
-							My journey in the world of coding began with a fascination for technology, and over
+							{`My journey in the world of coding began with a fascination for technology, and over
 							the years, I've honed my skills in Wordpress, laravel , reactjs, Nextjs, Flutter
 							..etc. Whether it's developing scalable applications, optimizing algorithms, or
-							troubleshooting intricate bugs, I approach each task with enthusiasm and dedication.
+							troubleshooting intricate bugs, I approach each task with enthusiasm and dedication.`}
 						</p>
 						<p>
-							In addition to my technical expertise, I pride myself on my ability to collaborate
+							{`In addition to my technical expertise, I pride myself on my ability to collaborate
 							effectively with cross-functional teams. I believe that communication is key to any
 							successful project, and I enjoy fostering a collaborative environment where ideas can
-							flourish.
+							flourish.`}
 						</p>
 						<p className="my-3.5">
-							Continuously staying abreast of the latest industry trends, I am committed to lifelong
+							{`Continuously staying abreast of the latest industry trends, I am committed to lifelong
 							learning and professional growth. My goal is not just to write code, but to create
 							elegant solutions that have a positive impact on end-users. Outside of the digital
 							realm, you might find me exploring new technologies, attending tech meetups, or
-							contributing to open-source projects.
+							contributing to open-source projects.`}
 						</p>
 						<p className="my-3.5">
-							If you're seeking a software engineer who is not only technically proficient but also
+							{`If you're seeking a software engineer who is not only technically proficient but also
 							brings creativity and a collaborative spirit to the table, then I'd love to connect
 							and discuss how I can contribute to your next exciting project. Let's build something
-							great together!
+							great together!`}
 						</p>
 					</div>
 				</div>
